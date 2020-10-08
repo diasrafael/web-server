d62a2494db6c77d151232ebfc03db772666d1855
Rafael Dias <rafael.dias@bemobi.com.br>
2020-10-08 09:24:20 -0300

    adding 404 handling

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,19 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
     stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    let (filename, status_code) = if buffer.starts_with(get) {
+        ("hello.html", "200 OK")
+    } else {
+        ("404.html", "404 NOT FOUND")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}", status_code, contents.len(), contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     println!("Response: {}", String::from_utf8_lossy(&response.as_bytes()));
 }
